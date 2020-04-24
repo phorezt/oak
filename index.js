0a94b446a28df6bf5f4dce37f86df90752742d7f
nikek <nikek@spotify.com>
2020-04-24 23:00:59 +0200

    add emoji to service response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("oak"));
+app.get("/", (req, res) => res.send("oak 🌳"));
 
 app.listen(port, () =>
   console.log(`Oak app listening at http://localhost:${port}`)
